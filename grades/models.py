903ca4e1f30d3c88e97a2ee134d44a476fa8a5e5
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-11 20:02:33 +0545

    Fixed Model with names

--- grades/models.py
+++ grades/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class StudentGrade(models.Model):
+    # Django automatically creates an 'id' primary key field, so you don’t need to define it manually
+    name = models.CharField(max_length=100)   # student name
     attendance = models.IntegerField()
     homework = models.IntegerField()
     test_score = models.IntegerField()
     final_grade = models.IntegerField()
 
     def __str__(self):
-        return f"Student {self.id} - Grade {self.final_grade}"
+        return f"{self.name} - Grade {self.final_grade}"
