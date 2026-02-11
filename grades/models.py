d2e0912e84e26a49dce310b4dbe7300cff4b43a5
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-11 19:49:05 +0545

    Created StudentGradeModel

--- grades/models.py
+++ grades/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class StudentGrade(models.Model):
+    attendance = models.IntegerField()
+    homework = models.IntegerField()
+    test_score = models.IntegerField()
+    final_grade = models.IntegerField()
+
+    def __str__(self):
+        return f"Student {self.id} - Grade {self.final_grade}"
